f249b407c11102ec6ff269470207d29bb63ff2c4
bouchardhenry <bouchardhenry@gmail.com>
2026-04-01 14:29:18 +0200

    added new validators

--- src/middleware/productValidation.js
+++ src/middleware/productValidation.js
@@ -2,11 +2,19 @@ import { body, validationResult } from "express-validator";
 
 export const validateProduct = [
   body("name").notEmpty().withMessage("Name is required"),
+  body("brand").notEmpty().withMessage("Brand is required"),
   body("price").isFloat({ min: 0 }).withMessage("Price must be greater than 0"),
-  body("stock").isInt({ min: 0 }).withMessage("Stock must be greater than 0"),
-  body("image").notEmpty().withMessage("Image is required"),
-  body("slug").notEmpty().withMessage("Slug is required"),
-  //TODO: Add more validation rules as needed
+  body("dropAt").isISO8601().withMessage("dropAt must be a valid date"),
+  body("dropEnd").optional().isISO8601().withMessage("dropEnd must be a valid date"),
+  body("sizes").isArray({ min: 1 }).withMessage("sizes must be a non-empty array"),
+  body("sizes.*.size").notEmpty().withMessage("Each size must have a size value"),
+  body("sizes.*.stock").isInt({ min: 0 }).withMessage("Each size must have a stock of 0 or more"),
+   body("description").optional().isString(),
+  body("color.name").optional().isString(),
+  body("color.hex")
+    .optional()
+    .matches(/^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/)
+    .withMessage("hex must be a valid color, e.g. #fff or #1a1a1a"),
 ];
 
 export const validateProductResult = (req, res, next) => {

f439ff445200c3c26e94e4f0ba62967e2f536d53
Estevivanco <esteban@vivanco.se>
2026-04-02 15:00:59 +0200

    added routes for users and admin

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,17 +1,28 @@
 import { Router } from "express";
-import { getProfile, getAllUsers, getUserById } from "../controller/UserController.js";
-import { authenticateToken} from "../middleware/auth.js";
+import { getProfile, getAllUsers, getUserById, makeAdmin, deactivateUser, updateUser, updateProfile, deleteProfile, reactivateUser, permanentDeleteUser} from "../controller/UserController.js";
+import { authenticateToken, requireAdmin} from "../middleware/auth.js";
 import {validateUpdateUser} from '../middleware/userValidation.js';
 
 const router = Router();
 
-
-router.get("/users/:id", getUserById);
-
-
-//admin
-router.get("/users", getAllUsers);
+//user routes
 router.get('/users/profile', authenticateToken, getProfile);
+router.put('/users/profile', authenticateToken, updateProfile);
+router.delete('/users/profile', authenticateToken, deleteProfile);
+
+//wishlist routes
+// router.get('/users/wishlist', authenticateToken, getWishlist);
+// router.post('/users/wishlist/:productId', authenticateToken, addToWishlist);
+// router.delete('/users/wishlist/:productId', authenticateToken, removeFromWishlist);
+
+//admin routes
+router.get('/users', authenticateToken, requireAdmin, getAllUsers);
+router.get('/users/:id', authenticateToken, requireAdmin, getUserById);
+router.put('/users/:id', authenticateToken, requireAdmin, validateUpdateUser, updateUser);
+router.delete('/users/:id/deactivate', authenticateToken, requireAdmin, deactivateUser);
+router.patch('/users/:id/reactivate', authenticateToken, requireAdmin, reactivateUser);
+router.delete('/users/:id/permanent', authenticateToken, requireAdmin, permanentDeleteUser);
+router.post('/users/:id/make-admin', authenticateToken, requireAdmin, makeAdmin);
 
 
 

333c31f3b4e62f2becb5e73ca6217cda5b6471f4
Estevivanco <esteban@vivanco.se>
2026-04-02 14:58:47 +0200

    added a require admin function

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -40,3 +40,18 @@ export const authenticateToken = (req, res, next) => {
     });
   }
 };
+
+export const requireAdmin = (req,res, next) => {
+  if(!req.user || !req.user.userId){
+    return res.status(401).json({
+      message: "Authentication required"
+    })
+  }
+
+  if(req.user.role !== 'admin'){
+    return res.status(403).json({
+      message: "Forbidden: Admin access required"
+    })
+  }
+  next()
+}

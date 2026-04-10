6a4443eaea3a3f7b9125205c26539e3e1b7d1581
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-04-10 14:22:43 +0200

    Added Resend email function

---
+++ src/utils/email.js
@@ -0,0 +1,46 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendOrderConfirmation = async (order, user) => {
+  await resend.emails.send({
+    from: "solesearch@sole.search",
+    to: user.email,
+    subject: `Order confirmation # ${order._id}`,
+    html: `
+            <h1>Thank you for shopping at Sole Search ${user.firstName}!</h1>
+            <p> Your order <i>#${order._id}</i> is being processed for shipping.</p>
+            <h2>Order Summary</h2>
+            <ul>
+                ${order.items
+                  .map(
+                    (item) => `
+                    <li>${item.product.name}, size ${item.size}: á ${item.unitPrice} kr</li>
+                    `,
+                  )
+                  .join("")}
+            </ul>
+            <p>Order Total: ${order.orderTotal} kr</p>
+            <p>
+              <a href="${process.env.FRONTEND_URL}/orders/${order._id}/track">
+                Track you order
+              </a>
+            </p>
+        `,
+    // Total price virtual
+    // Track my order
+  });
+};
+
+export const sendOrderCancellation = async (order, user) => {
+  await resend.emails.send({
+    from: "solesearch@sole.search",
+    to: user.email,
+    subject: `Your order #${order._id} has been cancelled.`,
+    html: `
+            <h1>Your order has been cancelled</h1>
+            <p>Hi ${user.firstName}, your order <i>#${order._id}</i> has been cancelled.</p>
+            <p>If you have any questions, please contact us.</p>
+        `,
+  });
+};

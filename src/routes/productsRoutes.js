c396395e09256e415234c165d3bba6e1223dfec9
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-04-16 14:32:25 +0200

    typo in file name

---
+++ src/routes/productsRoutes.js
@@ -0,0 +1,45 @@
+import { Router } from "express";
+import {
+  validateProduct,
+  validateProductUpdate,
+  validateProductResult,
+} from "../middleware/productValidation.js";
+import {
+  getProducts,
+  createProduct,
+  getProductBySlug,
+  updateProduct,
+  deleteProduct,
+} from "../controller/ProductController.js";
+import { authenticateToken, requireAdmin } from "../middleware/auth.js";
+import upload from "../middleware/imageUploader.js";
+
+const router = Router();
+
+router.get("/", getProducts);
+
+router.get("/:slug", getProductBySlug);
+
+router.post(
+  "/",
+  authenticateToken,
+  requireAdmin,
+  upload.array("images",4),
+  validateProduct,
+  validateProductResult,
+  createProduct,
+);
+
+router.put(
+  "/:slug",
+  authenticateToken,
+  requireAdmin,
+  upload.array("images",4),
+  validateProductUpdate,
+  validateProductResult,
+  updateProduct,
+);
+
+router.delete("/:slug", authenticateToken, requireAdmin, deleteProduct);
+
+export default router;

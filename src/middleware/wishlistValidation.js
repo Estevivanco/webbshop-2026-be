2910203ccbfab6a0e8de86dcce4364361b17e72b
ddaviddahlgren <ddaviddahlgren@gmail.com>
2026-04-23 07:19:51 +0200

    removed size from validator

--- src/middleware/wishlistValidation.js
+++ src/middleware/wishlistValidation.js
@@ -2,9 +2,6 @@ import { body, validationResult } from "express-validator";
 
 export const validateWishlist = [
     body("productId").notEmpty().isMongoId().withMessage("Valid product is required"),
-    body("size")
-        .isString().notEmpty()
-        .matches(/^\d+$/).withMessage("Size must be a number")
 ];
 
 export const validateWishlistResult = (req, res, next) => {
